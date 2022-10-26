dbc4508e0f4e4926bdb8faf22614f334a0e1bef7
bcdewitt <dewittbc@gmail.com>
2022-10-26 15:30:32 -0400

    5 files modified

--- src/shared/components/ui-view/ui-view.module.ts
+++ src/shared/components/ui-view/ui-view.module.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { UiHeadingModule } from '../_private';
 import { UiViewComponent } from './ui-view.component';
 
 @NgModule({
   declarations: [UiViewComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, UiHeadingModule],
   exports: [UiViewComponent],
 })
 export class UiViewModule {}
